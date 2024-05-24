2feb25a44c5a5666ce5554e7456808021fd49aa7
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-05-24 08:55:39 -0300

    feat: add theme provider in App and new variants css

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import './global.css'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
 
-      <RouterProvider router={router} />
+        <Toaster richColors />
+
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
